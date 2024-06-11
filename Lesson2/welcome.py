b51da0f36aaba42914361f27d9e8fa40125d1c10
nimaanmi <maansingh13@gmail.com>
2024-06-11 09:57:54 -0300

    welcome.py

---
+++ Lesson2/welcome.py
@@ -0,0 +1,24 @@
+line01 = '********************'                          # header / footer
+line02 = '*                  *'    
+line03 = '*     WELCOME!     *'
+
+# Let's start this with a blank line.
+print('')                                                # 2 single quotes without any space inside the print() function will print a blank line.
+print(line01)                                            # keeping the cursor at the end of the line press shift+alt+down_arrow, this will copy paste the current line below.
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+
+'''
+
+How to print:
+********************
+*                  *
+*     WELCOME!     *
+*                  *
+********************
+
+
+'''
