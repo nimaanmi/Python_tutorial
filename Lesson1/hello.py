75d094f265be91aa5328d88856651bfa3f8e5ed6
nimaanmi <maansingh13@gmail.com>
2024-06-11 09:47:33 -0300

    hello.py

--- Lesson1/hello.py
+++ Lesson1/hello.py
@@ -1,2 +1,12 @@
-greeting = 'Hello World'
+greeting = 'Hello World'      
 print(greeting)
+
+'''
+
+
+1. here we are creating a variable greeting and assigning a strings to it. Strings are alphabets and they are always inside two single quotes ' ' or double quotes " " 
+2. we are using the print() function to print the assigned value of the variable greeting.
+3. The output will be: Hello World
+
+
+'''
