7d66d7c003a017748590dd1c3ab074c67c167fb4
nimaanmi <maansingh13@gmail.com>
2024-06-11 10:45:09 -0300

    meaning.py

---
+++ Lesson3/meaning.py
@@ -0,0 +1,18 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#   print('Right on!')
+# else:
+#   print('Not today')
+
+# or we can write this in one line.
+print('Right on') if meaning > 10 else print('Not today')
+
+
+'''
+
+Output: Right on!
+
+
+'''
