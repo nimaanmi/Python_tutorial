88ca647e5e2fc196275265f34410169f6f48f24e
nimaanmi <maansingh13@gmail.com>
2024-07-19 13:06:32 -0300

    RPS2.py

--- Lesson8/RPS2.py
+++ Lesson8/RPS2.py
@@ -1,1 +1,52 @@
 
+# improving the RPS / Rock Paper Scissor game so we don't have to start it over and over again.
+
+import sys
+import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSOR = 3
+
+
+playagain = True
+while playagain:
+
+    playerchoice = input(
+        'Enter.......\n1 for Rock\n2 for Paper\n3 for Scissor\n\n')
+    player = int(playerchoice)
+
+    if player < 1 or player > 3:
+        sys.exit('You need to enter 1, 2, or 3')
+
+    computerchoice = random.choice('123')
+    computer = int(computerchoice)
+
+    print()
+    print('Player entered:', str(RPS(player)).replace('RPS.', ''))
+    print('Computer entered: ', str(RPS(computer)).replace('RPS.', ''))
+    print()
+
+    if player == 1 and computer == 3:
+        print('🎉Player wins!')
+    elif player == 2 and computer == 1:
+        print('🎉Player wins!')
+    elif player == 3 and computer == 2:
+        print('🎉Player wins!')
+    elif player == computer:
+        print('😱It\'s a draw!')
+    else:
+        print('🐍Computer wins!')
+
+    playagain = input('Play again? /nY for Yes\nQ for Quit\n\n')
+    if playagain.lower() == 'y':
+        continue
+    else:
+        print('🎉🎉🎉')
+        print('Thank your for playing!')
+        playagain = False
+
+sys.exit('Bye! 👋')
