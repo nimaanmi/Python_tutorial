a8bbfdb8a35a72dbe8c0857a2e0fef8b832a62a3
nimaanmi <maansingh13@gmail.com>
2024-08-03 11:11:48 -0300

    Update rps5.py

--- Lesson_12/rps5.py
+++ Lesson_12/rps5.py
@@ -29,8 +29,8 @@ def rps():
         computer = int(computerchoice)
 
         print()
-        print('\nPlayer entered: ' + str(RPS(player)).replace('RPS.', ''))
-        print('Python entered: ' + str(RPS(computer)).replace('RPS.', ''))
+        print('\nPlayer entered: ' + str(RPS(player)).replace('RPS.', '').title())
+        print('Python entered: ' + str(RPS(computer)).replace('RPS.', '').title())
         print()
 
         def decide_winner(player, computer):
