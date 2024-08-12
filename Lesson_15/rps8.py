a1450bc30764acff81311ddb99e086b4b97fa259
nimaanmi <maansingh13@gmail.com>
2024-08-11 21:47:50 -0300

    Update rps8.py

--- Lesson_15/rps8.py
+++ Lesson_15/rps8.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(
-        description='Provides a personalized geme experience.')
+        description='Provides a personalized game experience.')
     parser.add_argument(
         '-n', '--name', metavar='name',
         required=True, help='The name of the person playing the game.'
