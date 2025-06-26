4c752805bc55ca3214fa71a258b91b48b2eaa658
nimaanmi <maansingh13@gmail.com>
2025-06-26 09:06:46 -0300

    Update lists.py

--- Lesson6/lists.py
+++ Lesson6/lists.py
@@ -55,7 +55,7 @@ print(users.index('Sara'))
 Output:
 2
 
-# How to check a range of values?
+# How to check/get a range of values?
 
 print(users[0:])                     # prints all the times in the list from the first position.
 print(users[:2])                     # pritns the items from first position to 2nd postion, (excludes the thirst as it's inclusive)
