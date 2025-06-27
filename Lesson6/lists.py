577c88add8a0f67208c2d36fdd1dcc9b6bedbf76
nimaanmi <maansingh13@gmail.com>
2025-06-27 08:40:13 -0300

    Update lists.py

--- Lesson6/lists.py
+++ Lesson6/lists.py
@@ -95,8 +95,10 @@ Output:
 
 # Adding an existing list to a new list.
 
-users += ['Jason']                # 1st list = users, 2nd list has just one time called 'Jason'. So, here we have created a new list and add added to another list.
-print(users)                      # make sure to use the [] while creating the list, if we miss [] and just user 'Jason', it'll add each letter as an item.
+users += ['Jason']                
+print(users)                      
+# 1st list = users, 2nd list has just one time called 'Jason'. So, here we have created a new list and add added to another list.
+# make sure to use the [] while creating the list, if we miss [] and just user 'Jason', it'll add each letter as an item.
 
 Output:
 ['Dave', 'John', 'Sara', 'Elsa', 'Jason']
