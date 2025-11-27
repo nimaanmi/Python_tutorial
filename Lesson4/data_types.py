40c661550f3705398fedc188f75caa7c7ccea98f
nimaanmi <maansingh13@gmail.com>
2025-11-27 09:27:49 -0400

    Update data_types.py

--- Lesson4/data_types.py
+++ Lesson4/data_types.py
@@ -1,5 +1,5 @@
 # How to name a file in python. 
-# We always under small letters. To separate words we use underscore.. So, data_types.py
+# We always use small letters. To separate words we use underscore.. So, data_types.py
 
 # Data types: 
 # The first data type is String Data Type
